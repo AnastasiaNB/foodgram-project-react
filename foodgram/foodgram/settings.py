d2436d1c05d71c569e2b3c57ad4fd308c9607ee9
Anastasia <D:\Mail>
2023-01-10 15:02:01 +0300

    fixes 1 review

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -9,6 +9,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
+    }
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
