487a6eaadd47ec1e0c89545924f0f100e5209e1c
anastasia <anastasia.borovik.1998@mail.ru>
2023-01-10 19:53:25 +0300

    fixes review 1

--- foodgram/api/urls.py
+++ foodgram/api/urls.py
@@ -9,13 +9,16 @@ router = SimpleRouter()
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 router.register('tags', TagViewSet, basename='tags')
-router.register(r'recipes/(?P<recipe_id>\d+)/favorite', FavoriteViewSet, basename='favorite')
-router.register(r'recipes/(?P<recipe_id>\d+)/shopping_cart', ShoppingCartViewSet, basename='shoppingcart')
+router.register(
+    r'recipes/(?P<recipe_id>\d+)/favorite',
+    FavoriteViewSet, basename='favorite')
+router.register(
+    r'recipes/(?P<recipe_id>\d+)/shopping_cart',
+    ShoppingCartViewSet, basename='shoppingcart')
 router.register('users', CustomUserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('users/<int:user_id>/subscribe/', FollowPostDeleteViewSet.as_view()),
     path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-]
+    path('auth/', include('djoser.urls.authtoken')),]
